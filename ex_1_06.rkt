ea1edb877b0c73a89a331c33ad91fa031db03b37
Ariman <xarimanx@gmail.com>
2011-08-18 01:09:01 +0300

    six

--- ex_1_06.rkt
+++ ex_1_06.rkt
@@ -1,1 +1,16 @@
 #lang racket
+;так как if не особая форма следует попытка вычислить оба операнда одновременно, что приведет к зацикливанию 
+
+define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(new-if (= 2 3) 0 5)
+
+(new-if (= 1 1) 0 5)
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
